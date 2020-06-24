f713728a8f9b94dbaa645846959c11edc2189950
Pierre Pronchery <khorben@defora.org>
2020-06-24 20:57:50 +0200

    Make it work for my purpose

--- src/main.c
+++ src/main.c
@@ -1,7 +1,49 @@
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include "mpvd.h"
+
+#ifndef PROGNAME_MPVD
+# define PROGNAME_MPVD	"mpvd"
+#endif
+
+
+static int _error(char const * message);
 
 int main(int argc, char * argv[])
 {
+	char const * pidfile = "/var/run/mpvd.pid";
+	const int daemonize = 1;
+	const uid_t uid = 502;
+	const gid_t gid = 502;
+	FILE * fp;
+
+	if(daemonize && daemon(0, 0) != 0)
+	{
+		return _error("daemon");
+	}
+	if((fp = fopen(pidfile, "w")) == NULL)
+		_error(pidfile);
+	else
+	{
+		fprintf(fp, "%u\n", getpid());
+		if(fclose(fp) != 0)
+			_error(pidfile);
+	}
+	if(setegid(gid) != 0)
+		_error("setegid");
+	if(seteuid(uid) != 0)
+		_error("seteuid");
 	return mpvd(argc, argv);
+}
+
+static int _error(char const * message)
+{
+	fprintf(stderr, "%s%s%s: %s\n", PROGNAME_MPVD,
+			(message != NULL) ? ": " : "",
+			(message != NULL) ? message : "",
+			strerror(errno));
+	return 2;
 }
--- src/mpvd.c
+++ src/mpvd.c
@@ -7,8 +7,8 @@
 #endif
 
 
-static int _mpvd_error(int error);
-static int _mpvd_event(mpv_event * event);
+static int _mpvd_error(int error, char const * message);
+static int _mpvd_event(mpv_handle * mpv, mpv_event * event);
 
 int mpvd(int filec, char * filev[])
 {
@@ -16,40 +16,54 @@ int mpvd(int filec, char * filev[])
 	int error;
 	int t = 1;
 	int i;
-	const char * command[] = { "loadfile", NULL, NULL };
+	const char * command_loadfile[] = { "loadfile", NULL, "append", NULL };
 
 	if((mpv = mpv_create()) == NULL)
 		return 2;
 	if((error = mpv_set_property(mpv, "shuffle", MPV_FORMAT_FLAG, &t)) != 0)
-		_mpvd_error(error);
+		_mpvd_error(error, "shuffle");
 	if((error = mpv_initialize(mpv)) != 0)
 	{
 		mpv_destroy(mpv);
-		return _mpvd_error(error);
+		return _mpvd_error(error, NULL);
 	}
+	if((error = mpv_set_property(mpv, "idle", MPV_FORMAT_FLAG, &t)) != 0)
+		_mpvd_error(error, "idle");
 	for(i = 0; i < filec; i++)
 	{
-		command[1] = filev[i];
-		if((error = mpv_command(mpv, command)) != 0)
-			_mpvd_error(error);
+		command_loadfile[1] = filev[i];
+		if((error = mpv_command_async(mpv, 0, command_loadfile)) != 0)
+			_mpvd_error(error, filev[i]);
 	}
-	while(_mpvd_event(mpv_wait_event(mpv, -1)) == 0);
+	while(_mpvd_event(mpv, mpv_wait_event(mpv, -1)) == 0);
 	mpv_destroy(mpv);
 	return 0;
 }
 
-static int _mpvd_error(int error)
+static int _mpvd_error(int error, char const * message)
 {
-	fprintf(stderr, "%s: %s\n", PROGNAME_MPVD, mpv_error_string(error));
+	fprintf(stderr, "%s%s%s: %s\n", PROGNAME_MPVD,
+			(message != NULL) ? ": " : "",
+			(message != NULL) ? message : "",
+			mpv_error_string(error));
 	return 2;
 }
 
-static int _mpvd_event(mpv_event * event)
+static int _mpvd_event(mpv_handle * mpv, mpv_event * event)
 {
 	mpv_event_log_message * log_message;
+	char const * command_shuffle[] = { "playlist-shuffle", NULL };
+	char const * command_play[] = { "set", "playlist-pos", "0", NULL };
+	int error;
 
 	switch(event->event_id)
 	{
+		case MPV_EVENT_IDLE:
+			if((error = mpv_command(mpv, command_shuffle)) != 0)
+				_mpvd_error(error, "shuffle");
+			if((error = mpv_command(mpv, command_play)) != 0)
+				_mpvd_error(error, "play");
+			break;
 		case MPV_EVENT_LOG_MESSAGE:
 			log_message = event->data;
 			printf("%s: %s\n", PROGNAME_MPVD, log_message->text);
