33f4b7b521679846ca3e65caddf18c6c30a8cad1
Pierre Pronchery <khorben@defora.org>
2021-01-10 13:36:50 +0100

    Update the copyright notice

--- src/mpvd.c
+++ src/mpvd.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2020 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2020-2021 Pierre Pronchery <khorben@defora.org> */
 /* This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3 of the License.
